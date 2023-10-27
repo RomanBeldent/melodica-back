e378aa3d7097be794771ddfe8dd0c4a78c6b7a3d
RomanBeldent <roman.beldent@oclock.school>
2023-10-27 18:00:11 +0200

    WIP crud address

---
+++ src/Controller/Backoffice/AddressController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller\Backoffice;
+
+use App\Entity\Address;
+use App\Form\AddressType;
+use App\Repository\AddressRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("back/address", name="back_address_")
+ */
+class AddressController extends AbstractController
+{
+    /**
+     * @Route("/", name="list", methods={"GET"})
+     */
+    public function list(AddressRepository $addressRepository): Response
+    {
+        return $this->render('address/list.html.twig', [
+            'addresses' => $addressRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create", methods={"GET", "POST"})
+     */
+    public function create(Request $request, AddressRepository $addressRepository): Response
+    {
+        $address = new Address();
+        $form = $this->createForm(AddressType::class, $address);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $addressRepository->add($address, true);
+
+            return $this->redirectToRoute('back_address_list', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('address/create.html.twig', [
+            'address' => $address,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}", name="show", methods={"GET"})
+     */
+    public function show(Address $address): Response
+    {
+        return $this->render('address/show.html.twig', [
+            'address' => $address,
+        ]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}/edit", name="edit", methods={"GET", "PUT"})
+     */
+    public function edit(Request $request, Address $address, AddressRepository $addressRepository): Response
+    {
+        $form = $this->createForm(AddressType::class, $address);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $addressRepository->add($address, true);
+
+            return $this->redirectToRoute('back_address_list', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('address/edit.html.twig', [
+            'address' => $address,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id<\d+>}", name="delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Address $address, AddressRepository $addressRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$address->getId(), $request->request->get('_token'))) {
+            $addressRepository->remove($address, true);
+        }
+
+        return $this->redirectToRoute('back_address_list', [], Response::HTTP_SEE_OTHER);
+    }
+}
