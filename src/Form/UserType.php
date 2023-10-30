0a0f786af06f6464e8f5f8082ec6264daa8a90e1
RomanBeldent <roman.beldent@oclock.school>
2023-10-30 10:10:55 +0100

    starting crud-user pair programming

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +17,7 @@ class UserType extends AbstractType
             ->add('lastname')
             ->add('birthday')
             ->add('email')
-            ->add('password')
+            ->add('password', PasswordType::class)
             ->add('phone_number')
             ->add('picture')
             ->add('created_at')
