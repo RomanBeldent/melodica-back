cae361774d236eef840b307ce456ef039ddf730e
JordanJacquot <jordan.jacquot@oclock.school>
2023-10-31 17:49:23 +0100

    constraint on band

--- src/Form/BandType.php
+++ src/Form/BandType.php
@@ -2,34 +2,69 @@
 
 namespace App\Form;
 
+use App\Entity\Tag;
 use App\Entity\Band;
 use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Genre;
+use App\Entity\Address;
+use App\Form\AddressType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\DBAL\Types\TextType as TypesTextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class BandType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('area')
-            ->add('sample')
-            ->add('picture')
-            ->add('created_at')
-            ->add('updated_at')
+            ->add('name', TextType::class,[
+                'label'=>'Nom'
+            ])
+            ->add('description',TextType::class, [
+                'label'=>'Description'
+            ])
+            ->add('area', IntegerType::class,[
+                'label'=>'Zone de recherche autour de moi'
+            ])
+            ->add('sample',TextType::class,[
+                'label' => 'Echantillon de musique'
+            ])
+            ->add('picture',TextType::class,[
+                'label'=>'Photo de profil'
+            ])
+            ->add('created_at', DateTimeType::class, [
+                'input' => 'datetime_immutable',
+                'disabled' => true,
+                ])
+            ->add('updated_at', DateTimeType::class, [
+                'input' => 'datetime_immutable',
+                'disabled' => true,
+            ])
             ->add('users', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'firstname',
                 'multiple' => true])
-            ->add('events')
-            ->add('tags')
-            ->add('genre')
-            ->add('address')
-        ;
+            
+            ->add('tags', EntityType::class,[
+                'label'=> 'Qu\'êtes vous?',
+                'class' => Tag::class,
+                'multiple' => true,
+                'choice_label' => 'name'
+            ])
+            ->add('genre', EntityType::class,[
+                'label' => 'Votre genre de musique',
+                'choice_label' => 'name',
+                'class' => Genre::class,
+                'multiple' => true
+            ])
+            ->add('address', AddressType::class);
+            
     }
 
     public function configureOptions(OptionsResolver $resolver): void
