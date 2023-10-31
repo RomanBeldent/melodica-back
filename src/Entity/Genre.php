287f5520d665f0b4b373692b213cfcd30fd05173
JordanJacquot <jordan.jacquot@oclock.school>
2023-10-31 10:54:56 +0100

    Form genre and type constraint OK

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -2,13 +2,16 @@
 
 namespace App\Entity;
 
-use App\Repository\GenreRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\GenreRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=GenreRepository::class)
+ * @UniqueEntity("name", message = "Ce nom existe déjà") 
  */
 class Genre
 {
--- src/Form/GenreType.php
+++ src/Form/GenreType.php
@@ -6,14 +6,17 @@ use App\Entity\Genre;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class GenreType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('bands')
+        ->add('name', TextType::class,[
+            'label' => 'Nom du genre'
+        ])
+            
         ;
     }
 
