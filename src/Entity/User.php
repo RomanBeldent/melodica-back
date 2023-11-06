2487f69929e6c3852280d31bf1a81c329e19689a
JordanJacquot <jordan.jacquot@oclock.school>
2023-11-06 14:56:11 +0100

    api message ok

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -27,7 +27,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @ORM\Column(type="string", length=50)
-     * @Groups({"user_list", "user_show", "user_create", "user_update", "organizer_list", "organizer_show", "band_list", "band_show"})
+     * @Groups({"user_list", "user_show", "user_create", "user_update", "organizer_list", "organizer_show", "band_list", "band_show","message_list", "message_show","message_create"})
      * @Assert\NotBlank
      */
     private $firstname;
@@ -35,7 +35,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @ORM\Column(type="string", length=50)
      * @Assert\NotBlank
-     * @Groups({"user_list", "user_show", "user_create", "user_update", "organizer_list", "organizer_show", "band_list", "band_show"})
+     * @Groups({"user_list", "user_show", "user_create", "user_update", "organizer_list", "organizer_show", "band_list", "band_show","message_list", "message_show","message_create"})
      */
     private $lastname;
 
