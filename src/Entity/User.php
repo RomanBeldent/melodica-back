d36139cb7dc0f302ccf635d0ca02be0dbaa476f6
gaetane-heiderich <gaetane.heiderich@oclock.school>
2023-11-13 17:40:45 +0100

    organizer picture to front

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -75,7 +75,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * Undocumented variable
      * @ORM\Column(type="string", nullable=true)
-     * @Groups({"user_list", "user_show", "user_create", "user_update","band_list","band_show"})
+     * @Groups({"user_list", "user_show", "user_create", "user_update","band_list","band_show", "organizer_list", "organizer_show"})
      */
     private $picture;
 
