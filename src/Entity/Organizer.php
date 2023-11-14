156720de4530716e97f1846d6d1be1d9aa7fd2f1
JordanJacquot <jordan.jacquot@oclock.school>
2023-11-14 14:20:38 +0100

    add address in json user_show

--- src/Entity/Organizer.php
+++ src/Entity/Organizer.php
@@ -72,7 +72,7 @@ class Organizer
     /**
      * @ORM\OneToOne(targetEntity=Address::class, inversedBy="organizer", cascade={"persist", "remove"})
      * @ORM\JoinColumn(nullable=false)
-     * @Groups({"organizer_list", "organizer_show", "organizer_create", "organizer_update"})
+     * @Groups({"organizer_list", "organizer_show", "organizer_create", "organizer_update","user_show"})
      */
     private $address;
 
