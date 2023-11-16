afa4793f3727e98855007ac8630a116778c6efe6
JordanJacquot <jordan.jacquot@oclock.school>
2023-11-16 10:34:16 +0100

    commentary on random and randomAll

--- src/Controller/Api/BandController.php
+++ src/Controller/Api/BandController.php
@@ -47,22 +47,25 @@ class BandController extends AbstractController
      */
     public function random(BandRepository $bandRepository): JsonResponse
     {
+        // récupération de tous les bands existant via fonction findAll
         $bands = $bandRepository->findAll();
+        // si on ne récupère aucun band alors on envoie un message comme quoireprésenteil n'y a pas de band dans la Bdd
         if (count($bands) === 0) {
             $errorMessage = [
                 'message' => "No bands in database",
             ];
+        // on retourne la réponse en json
             return new JsonResponse($errorMessage, Response::HTTP_NOT_FOUND);
         }
 
         // on mélange les groupes
         shuffle($bands);
-
+        // on boucle sur le tableau randomBands qui contient les bands stockés dans $bands
         $randomBands = [];
         for ($randomBandToAdd = 1; $randomBandToAdd <= 30; $randomBandToAdd++) {
             $randomBands[] = $bands[$randomBandToAdd];
         }
-
+        // on retourne les données générées en json pour pouvoir l'envoyer au front
         return $this->json([
             'randomBands' => $randomBands], 200, [], ['groups' => 'band_random']);
     }
@@ -75,12 +78,13 @@ class BandController extends AbstractController
         // un tableau de groupes + on limite l'envoie de données
         $bands = $bandRepository->findAll();
         shuffle($bands);
+        // on veut pouvoir n'afficher que les 12 premiers résultats
         $bandsSlice = array_slice($bands,0,12,true);
         // un tableau d'organisateur + on limite l'envoie de données
         $organizers = $organizerRepository->findAll();
         shuffle($organizers);
         $organizersSlice = array_slice($organizers,0,12,true);
-
+        // on envoie nos données en json pour le front
          return $this->json([
             'organizers' => $organizersSlice,
             'bands' => $bandsSlice], 200, [], ['groups' => 'random_all']);
